6f9f8910a815b990068d46343b7ff510f451e241
Tenuka <tenuka0506@gmail.com>
2026-06-22 09:16:31 +0400

    Basic functionality and test code

--- code.py
+++ code.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+import os
+from google import genai
+
+key = os.getenv("API_KEY")
+client = genai.Client(api_key=key)
+
+chat = client.chats.create(model="gemini-2.5-flash")
+
+if __name__ == "__main__":
+    print("Chat with Gemini: \n")
+
+    while True:
+        message = input("You: ")
+        if message.lower() in ("quit", "exit"):
+            break
+
+        response = chat.send_message(message)
+        print("\nGemini:", response.text, "\n")
